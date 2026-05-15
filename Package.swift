0ef97fd28409da96b54e0ab140daa9c777af06b5
Sushil Sahoo <Sushil.Sahoo@harman.com>
2026-05-15 11:40:09 +0530

    Fix: lower swift-tools-version to 5.9 for Xcode compatibility

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
     name: "TextSummarizerKit",
     platforms: [
-        .iOS(.v26),
-        .macOS(.v26)
+        .iOS(.v18),
+        .macOS(.v15)
     ],
     products: [
         .library(
